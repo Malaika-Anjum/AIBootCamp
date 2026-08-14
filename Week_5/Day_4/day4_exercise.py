c7ce00403e10ce933024f2ba3352a7a51cd2cb66
Malaika Anjum <malaikaanjum007@gmail.com>
2026-08-14 16:54:29 +0500

    Code File Added

--- Week_5/Day_4/day4_exercise.py
+++ Week_5/Day_4/day4_exercise.py
@@ -3,6 +3,7 @@
 
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, classification_report
@@ -18,7 +19,12 @@ df = pd.DataFrame(X, columns=['Age', 'Salary'])
 df['Purchase'] = y
 
 # Split Data
-X_train, X_test, y_train, y_test = train_test_split(df[['Age', 'Salary']], df['Purchase'], test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(
+    df[['Age', 'Salary']], 
+    df['Purchase'], 
+    test_size=0.2, 
+    random_state=42
+    )
 
 # Train logistic regression model
 model = LogisticRegression()
@@ -34,12 +40,14 @@ print("Recall:", recall_score(y_test, y_pred))
 print("F1 Score:", f1_score(y_test, y_pred))
 print("\nClassification Report:\n", classification_report(y_test, y_pred))
 
-import matplotlib.pyplot as plt
 
 # Plot decision boundary
 x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
 y_min, y_max = X[:, 1].min() -1, X[:, 1].max() + 1
-xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.1), np.arange(y_min, y_max, 0.1))
+xx, yy = np.meshgrid(
+    np.arange(x_min, x_max, 0.1), 
+    np.arange(y_min, y_max, 0.1)
+    )
 
 # Predict probabilities for grid points
 Z = model.predict(np.c_[xx.ravel(), yy.ravel()])
